e86e0798c94a4d53230eaaf1549dd0125c4635fd
Manuel de Brito Fontes <aledbf@gmail.com>
2016-05-24 17:13:12 -0300

    Avoid external downloads

--- lib/binaries.sh
+++ lib/binaries.sh
@@ -17,49 +17,15 @@ install_nodejs() {
   fi
 
   echo "Downloading and installing node $version..."
-  local download_url="https://s3pository.heroku.com/node/v$version/node-v$version-$os-$cpu.tar.gz"
-  local code=$(curl "$download_url" --silent --fail --retry 5 --retry-max-time 15 -o /tmp/node.tar.gz --write-out "%{http_code}")
-  if [ "$code" != "200" ]; then
-    echo "Unable to download node $version; does it exist?" && false
+  #local download_url="https://s3pository.heroku.com/node/v$version/node-v$version-$os-$cpu.tar.gz"
+  #curl "$download_url" --silent --fail  --retry 5 --retry-max-time 15 -o /tmp/node.tar.gz || (echo "Unable to download node $version; does it exist?" && false)
+  if [ ! -f /tmp/node.js/node-v$version-$os-$cpu.tar.gz ]; then
+    tar xzf /tmp/node.js/latest-node-$os-$cpu.tar.gz -C /tmp
+  else
+    tar xzf /tmp/node.js/node-v$version-$os-$cpu.tar.gz -C /tmp
   fi
-  tar xzf /tmp/node.tar.gz -C /tmp
+
   rm -rf $dir/*
   mv /tmp/node-v$version-$os-$cpu/* $dir
   chmod +x $dir/bin/*
-}
-
-install_iojs() {
-  local version="$1"
-  local dir="$2"
-
-  if needs_resolution "$version"; then
-    echo "Resolving iojs version ${version:-(latest stable)} via semver.io..."
-    version=$(curl --silent --get  --retry 5 --retry-max-time 15 --data-urlencode "range=${version}" https://semver.herokuapp.com/iojs/resolve)
-  fi
-
-  echo "Downloading and installing iojs $version..."
-  local download_url="https://iojs.org/dist/v$version/iojs-v$version-$os-$cpu.tar.gz"
-  curl "$download_url" --silent --fail --retry 5 --retry-max-time 15 -o /tmp/node.tar.gz || (echo "Unable to download iojs $version; does it exist?" && false)
-  tar xzf /tmp/node.tar.gz -C /tmp
-  mv /tmp/iojs-v$version-$os-$cpu/* $dir
-  chmod +x $dir/bin/*
-}
-
-install_npm() {
-  local version="$1"
-
-  if [ "$version" == "" ]; then
-    echo "Using default npm version: `npm --version`"
-  else
-    if needs_resolution "$version"; then
-      echo "Resolving npm version ${version} via semver.io..."
-      version=$(curl --silent --get --retry 5 --retry-max-time 15 --data-urlencode "range=${version}" https://semver.herokuapp.com/npm/resolve)
-    fi
-    if [[ `npm --version` == "$version" ]]; then
-      echo "npm `npm --version` already installed with node"
-    else
-      echo "Downloading and installing npm $version (replacing version `npm --version`)..."
-      npm install --unsafe-perm --quiet -g npm@$version 2>&1 >/dev/null
-    fi
-  fi
 }
